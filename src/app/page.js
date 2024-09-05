c62cba1deb35a152ebcf56b53201e251f0c36f4d
Jake Darby <designdarby@gmail.com>
2024-09-05 15:20:39 +0100

    adding image to layout

--- src/app/page.js
+++ src/app/page.js
@@ -5,29 +5,146 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col items-center justify-center  px-4 py-8">
-        <Image
-          className="pt-10 max-w-full h-auto"
-          src={"/property-toolkit-logo.svg"}
-          alt="logo"
-          width={520}
-          height={200}
-        />
-        <h2 className="mt-6 text-2xl font-semibold text-center text-gray-800">
-          We hope you&apos;re enjoying the Landlord Investment Show
-        </h2>
-        <p className="mt-4 text-center text-gray-600 max-w-lg">
-          Get your Property Investment Edge and claim your first 14 days free
-          &amp; get our HMO Package for FREE for the first 90 days
-        </p>
-        <div className="w-full max-w-md mt-8">
-          <PlaceholderForm />
+      <main className="grid grid-cols-1 md:grid-cols-2 h-screen">
+        {/* Mobile Image with Overlay Content */}
+        <div className="relative block md:hidden h-[50vh] w-full">
+          <Image
+            className="object-cover w-full h-full"
+            src="/property.jpg" // Replace with actual image path
+            alt="Top Background Image"
+            layout="fill"
+          />
+          {/* Overlay Content */}
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-black bg-white/80 p-2">
+            <Image
+              src={"/property-toolkit-logo.svg"}
+              alt="logo"
+              width={300}
+              height={150}
+              className="mb-4"
+            />
+            <h2 className="text-2xl font-semibold text-center p-2">
+              We hope you&apos;re enjoying the Landlord Investment Show
+            </h2>
+            <p className="mt-2 text-center max-w-sm p-2">
+              Get your Property Investment Edge and claim your first 14 days
+              free &amp; get our HMO Package for FREE for the first 90 days
+            </p>
+          </div>
         </div>
-        <Footer />
+
+        {/* Content Below Overlay Image on Mobile */}
+        <div className="block md:hidden flex-col items-center justify-center w-full px-4 py-2">
+          <div className="w-full max-w-md mt-8">
+            <PlaceholderForm />
+          </div>
+          <Footer />
+        </div>
+
+        {/* Left Section for Desktop */}
+        <div className="hidden md:flex flex-col items-center justify-center px-4 py-8 md:px-8">
+          <Image
+            className="pt-10 max-w-full h-auto"
+            src={"/property-toolkit-logo.svg"}
+            alt="logo"
+            width={520}
+            height={200}
+          />
+          <h2 className="mt-6 text-2xl font-semibold text-center text-gray-800">
+            We hope you&apos;re enjoying the Landlord Investment Show
+          </h2>
+          <p className="mt-4 text-center text-gray-600 max-w-lg">
+            Get your Property Investment Edge and claim your first 14 days free
+            &amp; get our HMO Package for FREE for the first 90 days
+          </p>
+          <div className="w-full max-w-md mt-8">
+            <PlaceholderForm />
+          </div>
+          <Footer />
+        </div>
+
+        {/* Right Section - Full Page Split Placeholder Image for Desktop */}
+        <div className="hidden md:flex h-full w-full">
+          <Image
+            className="object-cover w-full h-full"
+            src="/property.jpg" // Replace with actual image path
+            alt="Placeholder Image"
+            width={1000}
+            height={1000}
+          />
+        </div>
       </main>
     </>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <>
+//       <main className="md:flex-row h-screen grid grid-cols-1 md:grid-cols-2">
+//         {/* Left Section */}
+//         <div className="flex flex-col items-center justify-center w-full px-4 py-8 md:px-8">
+//           <Image
+//             className="pt-10 max-w-full h-auto"
+//             src={"/property-toolkit-logo.svg"}
+//             alt="logo"
+//             width={520}
+//             height={200}
+//           />
+//           <h2 className="mt-6 text-2xl font-semibold text-center text-gray-800">
+//             We hope you&apos;re enjoying the Landlord Investment Show
+//           </h2>
+//           <p className="mt-4 text-center text-gray-600 max-w-lg">
+//             Get your Property Investment Edge and claim your first 14 days free
+//             &amp; get our HMO Package for FREE for the first 90 days
+//           </p>
+//           <div className="w-full max-w-md mt-8">
+//             <PlaceholderForm />
+//           </div>
+//           <Footer />
+//         </div>
+
+//         {/* Right Section - Full Page Split Placeholder Image */}
+//         <div className="h-full order-1">
+//           <Image
+//             className="object-cover md:h-full h-[50vh] hidden md:block"
+//             src="/property.jpg"
+//             alt="Placeholder Image"
+//             width={1000}
+//             height={1000}
+//           />
+//         </div>
+//       </main>
+//     </>
+//   );
+// }
+
+// export default function Home() {
+//   return (
+//     <>
+//       <main className="flex flex-col items-center justify-center  px-4 py-8">
+//         <Image
+//           className="pt-10 max-w-full h-auto"
+//           src={"/property-toolkit-logo.svg"}
+//           alt="logo"
+//           width={520}
+//           height={200}
+//         />
+//         <h2 className="mt-6 text-2xl font-semibold text-center text-gray-800">
+//           We hope you&apos;re enjoying the Landlord Investment Show
+//         </h2>
+//         <p className="mt-4 text-center text-gray-600 max-w-lg">
+//           Get your Property Investment Edge and claim your first 14 days free
+//           &amp; get our HMO Package for FREE for the first 90 days
+//         </p>
+//         <div className="w-full max-w-md mt-8">
+//           <PlaceholderForm />
+//         </div>
+//         <Footer />
+//       </main>
+//     </>
+//   );
+// }
 
 // export default function Home() {
 //   return (
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,24 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <div class="flex justify-between mt-20 pt-8 border-t-2 border-[#000] bottom-0 flex-col md:flex-row w-[80vw]">
-      <span class="font-[400] text-[14px]">
+    <div className="flex justify-between mt-20 pt-8 border-t-2 border-[#000] bottom-0 flex-col md:flex-row w-full">
+      <span className="font-[400] text-[14px]">
         2023 propertytoolkit. All right reserved.
       </span>
-      <div class="flex space-x-6">
-        <a class="font-[900] text-[14px] border-b-2 border-[#000]" href="">
+      <div className="flex space-x-6">
+        <Link
+          className="font-[900] text-[14px] border-b-2 border-[#000]"
+          href=""
+        >
           Privacy Policy
-        </a>
-        <a class="font-[900] text-[14px] border-b-2 border-[#000]" href="">
+        </Link>
+        <Link
+          className="font-[900] text-[14px] border-b-2 border-[#000]"
+          href=""
+        >
           Terms of Service
-        </a>
+        </Link>
       </div>
     </div>
   );
