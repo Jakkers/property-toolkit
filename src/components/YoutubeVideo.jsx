cfe4e5a992e7d799279847aa4554097f7e058825
Jake Darby <designdarby@gmail.com>
2024-09-19 15:59:23 +0100

    fixed issue with video aspect ratio

--- src/components/YoutubeVideo.jsx
+++ src/components/YoutubeVideo.jsx
@@ -1,17 +1,15 @@
-"use client";
-
 export default function YoutubeVideo() {
   return (
-    <iframe
-      width="560"
-      height="315"
-      src="https://www.youtube.com/embed/ywWHDD9sFK8?si=5dZeycZXIrQEKiG9"
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerPolicy="strict-origin-when-cross-origin"
-      allowFullScreen
-      className="w-full rounded-lg shadow-md"
-    ></iframe>
+    <div className="relative w-full" style={{ paddingBottom: "56.25%" }}>
+      <iframe
+        src="https://www.youtube.com/embed/ywWHDD9sFK8?si=5dZeycZXIrQEKiG9"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+        className="absolute top-0 left-0 w-full h-full rounded-lg shadow-md"
+      ></iframe>
+    </div>
   );
 }
